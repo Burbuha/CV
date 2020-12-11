b0391432b1f8a6bf438f16f4d50f6faa54c83498
Burbuha <burbovskaja@gmail.com>
2020-12-11 15:38:57 +0300

    fixed scroll

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
     //забираем идентификатор бока с атрибута href
     var id = $(this).attr("href"),
       //узнаем высоту от начала страницы до блока на который ссылается якорь
-      top = $(id).offset().top;
+      top = $(id).offset().top - 100;
     //анимируем переход на расстояние - top за 1500 мс
     $("body,html").animate({ scrollTop: top }, 1500);
   });
